9182ee923c1b1f6fc5f8f3bb8100747d0275208d
Sandesh Wani <sandeshw@sandeshw-mac.internal.synopsys.com>
2022-07-25 23:46:00 -0400

    Updated solution on 25th July 2022

--- basic-data-types/list-comprehensions/solution.py
+++ basic-data-types/list-comprehensions/solution.py
@@ -23,8 +23,16 @@ def main():
     print("++++++++++++++Start of Solution D+++++++++++++++++++++++")
     #Solution D: with using list comprehensions in Python.
     solutionD(x,y,z,n)
-    
-    
+    print()
+    print("++++++++++++++Start of Solution E+++++++++++++++++++++++")
+    #Solution E: Simple for loops in python. Added most recently on 25th July 2022
+    solutionE(x,y,z,n)
+    print()
+    print("++++++++++++++Start of Solution f+++++++++++++++++++++++")
+    #Solution F: Using list comprehensions. Added most recently on 25th July 2022
+    solutionF(x,y,z,n)
+    print()
+
 def solutionA(x,y,n):
     ar = []
     p = 0
@@ -35,12 +43,12 @@ def solutionA(x,y,n):
                 ar[p] = [i,j]
                 p += 1
     print(ar)
-            
+
 def solutionB(x,y,n):
     result = [[i,j] for i in range(x+1) for j in range(y+1) if (i + j) != n ]
     print(result)
-    
-    
+
+
 def solutionC(x,y,z,n):
     ar = []
     p = 0
@@ -52,11 +60,24 @@ def solutionC(x,y,z,n):
                     ar[p] = [i,j,k]
                     p += 1
     print(ar)
-    
+
 def solutionD(x,y,z,n):
     result = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k) != n ]
     print(result)
-    
+
+def solutionE(x,y,z,n):
+    mylist = list()
+    for i in range(0, x+1):
+        for j in range(0, y+1):
+            for k in range(0, z+1):
+                if i+j+k != n:
+                    mylist.append([i,j,k])
+    print(mylist)
+
+def solutionF(x,y,z,n):
+    mylist = [ [i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n ]
+    print(mylist)
+
 
 if __name__ == '__main__':
     main()
--- introduction/if-else/solution.py
+++ introduction/if-else/solution.py
@@ -8,9 +8,7 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    if n%2 != 0:
+    if n % 2 != 0 or (n % 2 == 0 and 20 >= n >=6):
         print("Weird")
-    elif (n>=2 and n<=5) or n >20:
+    elif n % 2 == 0 and (n >= 2 and n <=5) or (n > 20):
         print("Not Weird")
-    elif (n>=6 and n<=20):
-        print("Weird")
